719626ac4e75c9b086cd50c1371bf8c4d845b30c
Björn Feber <bfeber@protonmail.com>
2023-03-29 23:13:32 +0200

    Only make GG work on prop_physics

--- game/hlvr/scripts/vscripts/check_useextra_distance.lua
+++ game/hlvr/scripts/vscripts/check_useextra_distance.lua
@@ -1,9 +1,9 @@
 local ents = Entities:FindAllInSphere(Entities:GetLocalPlayer():EyePosition(), 82)
 if thisEntity:Attribute_GetIntValue("picked_up", 0) == 0 and vlua.find(ents, thisEntity) then
     DoEntFireByInstanceHandle(thisEntity, "RunScriptFile", "useextra", 0, nil, nil)
-else
+elif thisEntity:GetClassname() == "prop_physics" then
     local direction = Entities:GetLocalPlayer():EyePosition() - thisEntity:GetAbsOrigin()
-    thisEntity:ApplyAbsVelocityImpulse(Vector(direction.x * 2, direction.y * 2, Clamp(direction.z * 4, -400, 400)))
+    thisEntity:ApplyAbsVelocityImpulse(Vector(direction.x * 2, direction.y * 2, Clamp(direction.z * 3.8, -400, 400)))
     StartSoundEventFromPosition("Grabbity.HoverPing", Entities:GetLocalPlayer():EyePosition())
     StartSoundEventFromPosition("Grabbity.Grab", Entities:GetLocalPlayer():EyePosition())
     DoEntFireByInstanceHandle(thisEntity, "use", "", 0.3, Entities:GetLocalPlayer(), nil)
