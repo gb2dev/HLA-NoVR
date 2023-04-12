e44702965addb537580714d20e37c676aa83f1ee
Björn Feber <bfeber@protonmail.com>
2023-04-12 14:05:51 +0200

    Smoother interaction

--- game/hlvr/scripts/vscripts/novr.lua
+++ game/hlvr/scripts/vscripts/novr.lua
@@ -496,6 +496,10 @@ if GlobalSys:CommandLineCheck("-novr") then
             SendToConsole("sk_plr_dmg_ar2 9")
             SendToConsole("sk_plr_dmg_smg1 5")
             SendToConsole("bind h +covermouth")
+            SendToConsole("player_use_radius 40")
+            SendToConsole("hlvr_physcannon_forward_offset 0")
+            -- TODO: Lower this when picking up very low mass objects
+            SendToConsole("player_throwforce 500")
 
             ent = Entities:FindByName(nil, "lefthand")
             if not ent then
