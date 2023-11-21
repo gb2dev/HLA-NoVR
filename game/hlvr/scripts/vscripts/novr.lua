46a1762593c1678ec1fe3e9e106e339a6e4033af
Björn Feber <bfeber@protonmail.com>
2023-11-21 13:43:06 +0100

    Fix opening crashed train door twice

--- game/hlvr/scripts/vscripts/novr.lua
+++ game/hlvr/scripts/vscripts/novr.lua
@@ -454,11 +454,12 @@ if GlobalSys:CommandLineCheck("-novr") then
 
                 ent = Entities:FindByClassnameNearest("info_hlvr_holo_hacking_plug", traceTable.pos, 10)
                 if ent then
+                    local name = ent:GetName()
                     local parent = ent:GetMoveParent()
-                    if ent:Attribute_GetIntValue("used", 0) == 0 and not (parent and vlua.find(parent:GetName(), "Console")) then
+                    if ent:Attribute_GetIntValue("used", 0) == 0 and not (parent and vlua.find(parent:GetName(), "Console")) and name ~= "traincar_01_hackplug" then
                         ent:Attribute_SetIntValue("used", 1)
                         DoEntFireByInstanceHandle(ent, "BeginHack", "", 0, nil, nil)
-                        if not vlua.find(ent:GetName(), "cshield") and not vlua.find(ent:GetName(), "switch_box") then
+                        if not vlua.find(name, "cshield") and not vlua.find(name, "switch_box") then
                             DoEntFireByInstanceHandle(ent, "EndHack", "", 1.8, nil, nil)
                             ent:FireOutput("OnHackSuccess", nil, nil, nil, 1.8)
                             ent:FireOutput("OnPuzzleSuccess", nil, nil, nil, 1.8)
@@ -649,7 +650,7 @@ if GlobalSys:CommandLineCheck("-novr") then
 
         if not loading_save_file and GlobalSys:CommandLineCheck("-noversioninfo") == false then
             -- Script update date and time
-            DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Nov 21 12:34", 255, 255, 255, 255, 999999)
+            DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Nov 21 12:43", 255, 255, 255, 255, 999999)
         end
 
         if GetMapName() == "startup" then
