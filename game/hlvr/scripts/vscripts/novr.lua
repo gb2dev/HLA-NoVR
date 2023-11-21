5f1b357ad4752608af4d38bd61a517e2fb563af0
Björn Feber <bfeber@protonmail.com>
2023-11-21 13:23:27 +0100

    Prevent pressing combine console plug

--- game/hlvr/scripts/vscripts/novr.lua
+++ game/hlvr/scripts/vscripts/novr.lua
@@ -453,7 +453,8 @@ if GlobalSys:CommandLineCheck("-novr") then
                 end
 
                 ent = Entities:FindByClassnameNearest("info_hlvr_holo_hacking_plug", traceTable.pos, 10)
-                if ent and ent:Attribute_GetIntValue("used", 0) == 0 then
+                local parent = ent:GetMoveParent()
+                if ent and ent:Attribute_GetIntValue("used", 0) == 0 and not (parent and vlua.find(parent:GetName(), "Console")) then
                     ent:Attribute_SetIntValue("used", 1)
                     DoEntFireByInstanceHandle(ent, "BeginHack", "", 0, nil, nil)
                     if not vlua.find(ent:GetName(), "cshield") and not vlua.find(ent:GetName(), "switch_box") then
@@ -646,7 +647,7 @@ if GlobalSys:CommandLineCheck("-novr") then
 
         if not loading_save_file and GlobalSys:CommandLineCheck("-noversioninfo") == false then
             -- Script update date and time
-            DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Nov 21 12:15", 255, 255, 255, 255, 999999)
+            DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Nov 21 12:23", 255, 255, 255, 255, 999999)
         end
 
         if GetMapName() == "startup" then
