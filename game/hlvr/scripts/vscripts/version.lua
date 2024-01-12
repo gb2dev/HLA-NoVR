01d3ad203a90eb123d292c935520bc15765f5b56
Björn Feber <bfeber@protonmail.com>
2024-01-12 21:04:03 +0100

    Undo tutorial background color change

--- game/hlvr/scripts/vscripts/version.lua
+++ game/hlvr/scripts/vscripts/version.lua
@@ -1,4 +1,4 @@
 if not loading_save_file and GlobalSys:CommandLineCheck("-noversioninfo") == false then
     -- Script update date and time
-    DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Jan 12 20:59", 255, 255, 255, 255, 999999)
+    DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Jan 12 21:04", 255, 255, 255, 255, 999999)
 end
