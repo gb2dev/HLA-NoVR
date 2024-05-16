69dcaf8a58661ea1ee59c4f32c73d805ddfea85e
Björn Feber <bfeber@protonmail.com>
2024-05-16 12:37:31 +0200

    Delete a5_vault_novr.vmap

--- game/hlvr/scripts/vscripts/version.lua
+++ game/hlvr/scripts/vscripts/version.lua
@@ -1,4 +1,4 @@
 if not loading_save_file and GlobalSys:CommandLineCheck("-noversioninfo") == false then
     -- Script update date and time
-    DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: May 16 12:36", 255, 255, 255, 255, 999999)
+    DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: May 16 12:37", 255, 255, 255, 255, 999999)
 end
