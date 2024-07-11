d8e1c11ac214f1efbba2e570d9699b0d5c5539ae
Björn Feber <bfeber@protonmail.com>
2024-07-11 13:44:40 +0200

    Change drainage wheel location

--- game/hlvr/scripts/vscripts/novr.lua
+++ game/hlvr/scripts/vscripts/novr.lua
@@ -1789,7 +1789,7 @@ if GlobalSys:CommandLineCheck("-novr") then
 
                     if GetMapName() == "a2_drainage" then
                         if not loading_save_file then
-                            Entities:FindByName(nil, "wheel2_physics"):SetOrigin(Vector(307, -2502, 380))
+                            Entities:FindByName(nil, "wheel2_physics"):SetOrigin(Vector(208, -2581, 408))
 
                             SendToConsole("ent_fire math_count_wheel2_installment AddOutput \"OnChangedFromMin>relay_install_wheel2>Trigger>>0>1\"")
                             SendToConsole("ent_fire math_count_wheel_installment AddOutput \"OnChangedFromMin>relay_install_wheel>Trigger>>0>1\"")
