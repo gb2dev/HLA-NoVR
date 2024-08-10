b80a127dc93cb0650f5bec9df741858f292a07f8
Björn Feber <bfeber@protonmail.com>
2024-08-10 12:07:35 +0200

    Improve grenades tutorial

--- game/hlvr/scripts/vscripts/version.lua
+++ game/hlvr/scripts/vscripts/version.lua
@@ -1,4 +1,4 @@
 if not loading_save_file and GlobalSys:CommandLineCheck("-noversioninfo") == false then
     -- Script update date and time
-    DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Aug 10 11:41", 255, 255, 255, 255, 999999)
+    DebugDrawScreenTextLine(5, GlobalSys:CommandLineInt("-h", 15) - 10, 0, "NoVR Version: Aug 10 12:07", 255, 255, 255, 255, 999999)
 end
